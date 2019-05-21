d9a15d217b83002ecaed420e837cb7255fba2cd8
Levon Mikaelyan <levon.mikaelyan@benivo.com>
2019-05-21 20:27:09 +0400

    First commit

---
+++ Lessons.Payment/PayPal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lessons.Payment
+{
+    public class PayPal
+    {
+        public static void Pay(int amount)
+        {
+            // Console.WriteLine("Paid " + amount + " succesfylly with PayPal!");
+            //Console.WriteLine("Paid {0} succesfylly with PayPal!", amount);
+            Console.WriteLine($"Paid ${amount} succesfylly with PayPal!");
+        }
+
+    }
+}
---
+++ Lessons/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Lessons.Payment;
+
+namespace Lessons
+{
+    class Program
+    {
+        static void Main()
+        {
+           Console.WriteLine("Hellow World");
+
+            PayPal.Pay(100);
+            PayPal.Pay(50);
+        }
+    }
+}
