ca819cf798e080c3f7c0de2a66407c676fd453c6
Levon Mikaelyan <levon.mikaelyan@benivo.com>
2019-05-23 20:52:37 +0400

    Types

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -7,12 +7,43 @@ namespace Lessons
     {
         static void Main()
         {
-           Console.WriteLine("Hellow Otter");
+            bool isMarried = false; // 1 bit
 
-            PayPal.Pay(100);
-            PayPal.Pay(50);
-            PayPal.Pay(500);
-            PayPal.Pay(500);
+            Console.WriteLine(isMarried.ToString().ToLower());
+
+            string name = "Armen"; // 2 byte for each character - 10 byte == 80 bit
+
+            char symbol =  'a' // 2 byte
+
+            byte color = 255; // 1 byte == 8 bit 
+
+            int id = 1000;  // 4 byte == 32 bit (Int 32)
+
+            short id1 = 1234; // 2 byte == 16 bit (Int16)
+
+            long id2 = 321323123123313;  // 8 byte == 64 bit (Int64)
+
+            float size = 3.12f; // 4 byte
+
+            double height = 4.32; // 8 byte
+
+           decimal amount = 3.123m; // 16 byte
+
+       
+
+            DateTime birthday = DateTime.Now;
+
+
+
+
+            Console.WriteLine();
+
+
+
+          
+
+                
+           
         }
     }
 }
